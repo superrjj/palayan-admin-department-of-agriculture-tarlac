99664433b7c12caacaebf65d09cce856f9c1f2de
John Harvee Quirido <quiridojohnharvee@gmail.com>
2025-09-25 02:06:22 +0800

    Change the limit of upload pictures in rice disease

--- src/components/DiseaseManagement/AddDiseaseModal.jsx
+++ src/components/DiseaseManagement/AddDiseaseModal.jsx
@@ -30,8 +30,8 @@ const AddDiseaseModal = ({ onClose, onSave, diseaseData = null }) => {
     const files = Array.from(e.target.files);
     console.log("Selected files:", files);
     
-    if (files.length + images.length > 1000) { //Reduced limit for testing
-      alert("You can upload a maximum of 1000 images.");
+    if (files.length + images.length > 2000) { //Reduced limit for testing
+      alert("You can upload a maximum of 2000 images.");
       return;
     }
     setImages((prev) => [...prev, ...files]);
@@ -87,8 +87,8 @@ const AddDiseaseModal = ({ onClose, onSave, diseaseData = null }) => {
       return;
     }
 
-    if (!isEdit && images.length === 300) {
-      alert("Please upload at least 300 image for new diseases.");
+    if (!isEdit && images.length === 800) {
+      alert("Please upload at least 800 image for new diseases.");
       return;
     }
 
@@ -240,7 +240,7 @@ const AddDiseaseModal = ({ onClose, onSave, diseaseData = null }) => {
             <label className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-lg p-5 cursor-pointer hover:border-green-500 transition">
               <UploadCloud className="w-10 h-10 text-gray-400 mb-2" />
               <span className="text-gray-500 text-sm text-center">
-                Click or drag files here {isEdit ? "(Optional)" : "(Required - Max 1000 images)"}
+                Click or drag files here {isEdit ? "(Optional)" : "(Required - Max 2000 images)"}
               </span>
               <input 
                 type="file" 
